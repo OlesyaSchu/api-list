b502b3e22dd4a8256cf0dc52fe94701e319ed3a6
Olesya Schukina <olesyaschu@yandex.ru>
2022-02-18 00:05:17 +0300

    Add routes for Home and Favorite

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,31 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/views/Home.vue'
+import Favorites from '@/views/Favorites.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/list',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/favorites',
+    name: 'Favorites',
+    component: Favorites,
   },
-];
+  {
+    path: '*',
+    redirect: { name: 'Home' },
+  },
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
-});
+})
 
-export default router;
+export default router
