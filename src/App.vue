144c1bb46d37c430aa66b8ae86ee892ca02bb6cf
Olesya Schukina <olesyaschu@yandex.ru>
2022-02-18 00:02:54 +0300

    Add views Home and Favorites

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/list">Список API</router-link> |
+      <router-link to="/favorites">Избранное</router-link>
     </div>
     <router-view />
   </div>
