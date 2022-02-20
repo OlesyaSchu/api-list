e16ac67aa6304eabfbaade6ae14d90e95dc5cd5b
Olesya Schukina <olesyaschu@yandex.ru>
2022-02-20 14:12:06 +0300

    Add List component

---
+++ src/components/List/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ list }}</p>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  mounted() {
+    this.$store.dispatch('list/loadFullList')
+  },
+  computed: {
+    ...mapGetters({
+      list: 'list/getList',
+    }),
+  },
+}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="home">
     <h1>Список API</h1>
+    <List />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import List from '@/components/List'
 
 export default {
   name: 'Home',
-  mounted() {
-    this.$store.dispatch('list/loadFullList')
-  },
-  computed: {
-    ...mapGetters({
-      list: 'list/getList',
-    }),
+  components: {
+    List,
   },
 }
 </script>
