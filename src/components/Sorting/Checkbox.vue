caed26183f85d8df4de4d223a30347886578c7f7
Olesya Schukina <olesyaschu@yandex.ru>
2022-02-21 03:35:15 +0300

    Add Checkbox component

---
+++ src/components/Sorting/Checkbox.vue
@@ -0,0 +1,29 @@
+<template>
+  <span @click="$emit('changeFilterValue')" class="checkbox">
+    <icheckbox :checked="filter.value" />
+    <p class="name">{{ filter.name }}</p>
+  </span>
+</template>
+
+<script>
+import icheckbox from '@/components/icons/checkbox'
+export default {
+  props: {
+    filter: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: {
+    icheckbox,
+  },
+}
+</script>
+
+<style lang="scss">
+.checkbox {
+  .name {
+    display: inline;
+  }
+}
+</style>
